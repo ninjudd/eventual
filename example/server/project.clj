32522f17ebb6868205deab3936f7ee5f39b3b10d
Justin Balthrop <git@justinbalthrop.com>
2014-01-29 15:02:54 -0800

    update ring-async to 0.2.3

--- example/server/project.clj
+++ example/server/project.clj
@@ -2,7 +2,7 @@
   :description "Example Server-Sent Events server using eventual."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.ninjudd/ring "1.2.2"]
-                 [com.ninjudd/ring-async "0.2.3-SNAPSHOT"]
+                 [com.ninjudd/ring-async "0.2.3"]
                  [com.ninjudd/eventual "0.1.1-SNAPSHOT"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [compojure "1.1.5"]
