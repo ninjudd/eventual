8f8c2a7ab7f980879c9d2692605113ac0dcf812f
Justin Balthrop <git@justinbalthrop.com>
2014-05-31 17:12:36 -0700

    0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ninjudd/eventual "0.3.1"
+(defproject com.ninjudd/eventual "0.3.2"
   :description "Server-Sent Event and EventSource helpers for core.async."
   :url "http://github.com/ninjudd/eventual"
   :license {:name "Eclipse Public License"
