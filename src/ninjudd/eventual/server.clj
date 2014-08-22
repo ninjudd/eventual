5c724016a46ad2d2582f4a6af63d4cef0019d5f8
Justin Balthrop <git@justinbalthrop.com>
2014-08-21 18:29:23 -0700

    switch to putting bytes on channel instead of string. detect out channel close so we can propagate it

--- src/ninjudd/eventual/server.clj
+++ src/ninjudd/eventual/server.clj
@@ -7,28 +7,32 @@
              "Content-Type" "text/event-stream; charset=utf-8"
              "Cache-Control" "no-cache"))
 
+(defn data [& args]
+  (.getBytes (apply str args) "UTF-8"))
+
 (defn sse-channel [f events & [opts]]
   (let [out (chan)
         disconnect (if-let [t (:timeout opts)]
                      (timeout t)
                      (chan))]
     (go (when-let [retry (:client-retry opts)]
-          (>! out (str "retry: " retry "\n")))
+          (>! out (data "retry: " retry "\n")))
         (loop []
-          (let [keepalive (timeout (:keepalive opts 25000))
-                [event ch] (alts! [events keepalive disconnect])]
-            (cond
-             (= ch events)
-             (when event
-               (when-let [event-id (:event-id (meta event))]
-                 (>! out (str "id: " event-id "\n")))
-               (when-let [event-type (:event-type (meta event))]
-                 (>! out (str "event: " (name event-type) "\n")))
-               (>! out (str "data: " (f event) "\n\n"))
-               (recur))
-             (= ch keepalive)
-             (do (>! out ":keepalive\n")
-                 (recur)))))
+          (when-not @(.closed out)
+            (let [keepalive (timeout (:keepalive opts 25000))
+                  [event ch] (alts! [events keepalive disconnect])]
+              (cond
+               (= ch events)
+               (when event
+                 (when-let [event-id (:event-id (meta event))]
+                   (>! out (data "id: " event-id "\n")))
+                 (when-let [event-type (:event-type (meta event))]
+                   (>! out (data "event: " (name event-type) "\n")))
+                 (>! out (data "data: " (f event) "\n\n"))
+                 (recur))
+               (= ch keepalive)
+               (do (>! out (data ":keepalive\n"))
+                   (recur))))))
         (close! events)
         (close! out))
     out))
