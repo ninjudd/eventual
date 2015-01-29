f22ee9d779b61d581ad6f4a4dcc8fd10237faa60
Justin Balthrop <git@justinbalthrop.com>
2015-01-29 11:33:55 -0800

    add support for subscribing to multiple events and event metadata from event-source

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Server-Sent Event and EventSource helpers for core.async.
 
 To use eventual, add the following to your `:dependencies`:
 
-    [com.ninjudd/eventual "0.1.0"]
+    [com.ninjudd/eventual "0.5.0"]
 
 ## Server
 
@@ -51,9 +51,27 @@ helper function that returns a core.async channel but user `EventSource` behind 
           (recur ...)))))
 ```
 
+## Event Metadata
+
+By default, only `message` events are sent and received. However, you can send diffent event types
+by providing metadata on the events you put onto the channel. You can also provide an event id this
+way.
+
+```clj
+(go ...
+  (>! events (with-meta event {:event-type :add, :event-id 1234})))
+```
+
+To receive events besides `message` on the client, you have to provide a list of events you
+handle. Events taken from the channel will have metadata with their type and id.
+
+```clj
+(edn-events "http://.../events" [:add :remove])
+```
+
 ## License
 
-Copyright © 2014 Justin Balthrop
+Copyright © 2015 Justin Balthrop
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- src/ninjudd/eventual/client.cljs
+++ src/ninjudd/eventual/client.cljs
@@ -2,20 +2,24 @@
   (:require [cljs.core.async :as async]
             [cljs.reader :refer [read-string]]))
 
-(defn event-source [f url]
-  (let [source (new js/EventSource url)
-        channel (async/chan)]
-    (.addEventListener source "message"
-                       (fn [e]
-                         (async/put! channel (f (.-data e)))))
+(defn event-source [f url & [event-types]]
+  (let [event-types (or event-types [:message])
+        source (new js/EventSource url)
+        channel (async/chan)
+        handler (fn [e]
+                  (async/put! channel (-> (f (.-data e))
+                                          (vary-meta merge {:event-id (.-lastEventId e)
+                                                            :event-type (keyword (.-type e))}))))]
+    (doseq [event-type event-types]
+      (.addEventListener source (name event-type) handler))
     {:event-source source
      :channel channel}))
 
-(defn edn-event-source [url]
-  (event-source read-string url))
+(defn edn-event-source [url & [event-types]]
+  (event-source read-string url event-types))
 
-(defn json-event-source [url]
-  (event-source #(.parse js/JSON %) url))
+(defn json-event-source [url & [event-types]]
+  (event-source #(.parse js/JSON %) url event-types))
 
 (defn close-event-source! [{:keys [channel event-source]}]
   (when event-source
