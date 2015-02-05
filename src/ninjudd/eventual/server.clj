bfcf70f7115b29946902528ad2b3dec5a80cdc7a
Justin Balthrop <git@justinbalthrop.com>
2015-02-05 10:26:14 -0800

    add :aggregate-event-id server option, a function that takes event-id and last-event-id this allows you to do things like only send event-id when it increases

--- src/ninjudd/eventual/server.clj
+++ src/ninjudd/eventual/server.clj
@@ -12,29 +12,34 @@
 
 (defn sse-channel [f events & [opts]]
   (let [out (chan)
+        aggregate (:aggregate-event-id opts)
         disconnect (if-let [t (:timeout opts)]
                      (timeout t)
                      (chan))]
     (go (when-let [retry (:client-retry opts)]
           (>! out (data "retry: " retry "\n")))
-        (loop []
+        (loop [last-event-id nil]
           (when-not @(.closed out)
             (let [keepalive (timeout (:keepalive opts 25000))
                   [event ch] (alts! [events keepalive disconnect])]
               (cond
                (= ch events)
                (when event
-                 (when-let [event-id (:event-id (meta event))]
-                   (>! out (data "id: " event-id "\n")))
-                 (when-let [event-type (:event-type (meta event))]
-                   (>! out (data "event: " (name event-type) "\n")))
-                 (>! out (data "data: " (f event) "\n\n"))
-                 (>! out :flush)
-                 (recur))
+                 (let [event-id (:event-id (meta event))
+                       event-id (if (and aggregate event-id LAST-event-id)
+                                  (aggregate event-id last-event-id)
+                                  event-id)]
+                   (when event-id
+                     (>! out (data "id: " event-id "\n")))
+                   (when-let [event-type (:event-type (meta event))]
+                     (>! out (data "event: " (name event-type) "\n")))
+                   (>! out (data "data: " (f event) "\n\n"))
+                   (>! out :flush)
+                   (recur (or event-id last-event-id))))
                (= ch keepalive)
                (do (>! out (data ":keepalive\n"))
                    (>! out :flush)
-                   (recur))))))
+                   (recur last-event-id))))))
         (close! events)
         (close! out))
     out))
